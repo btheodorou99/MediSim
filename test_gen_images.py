0d7eca9ea8d93f1d4708aee128815750a0ddbce8
btheodorou99 <99348934+btheodorou99@users.noreply.github.com>
2023-11-13 10:23:11 -0600

    Update test_gen_images.py

--- test_gen_images.py
+++ test_gen_images.py
@@ -9,7 +9,7 @@ from config_image import MediSimConfig
 from model_image import DiffusionModel
 
 SEED = 4
-cudaNum = 0
+cudaNum = 4
 NUM_SAMPLES = 25
 random.seed(SEED)
 np.random.seed(SEED)
@@ -22,7 +22,9 @@ if torch.cuda.is_available():
 test_dataset = pickle.load(open('./data_image/testDataset.pkl', 'rb'))
 test_dataset = [v for p in test_dataset for v in p['visits']]
 test_dataset = [(v[0], v[1]) for v in test_dataset]
+indexToCode = pickle.load(open('./data_image/indexToCode.pkl', 'rb'))
 image_transform = transforms.Compose([
+        transforms.Resize((config.image_dim_gen, config.image_dim_gen)),
         transforms.ToTensor(),
         transforms.Lambda(lambda x: 2*x - 1)  # Normalize to [-1, 1]
     ])
@@ -35,13 +37,15 @@ def get_batch(dataset, loc, batch_size):
     images = dataset[loc:loc+batch_size]
     bs = len(images)
     batch_context = torch.zeros(bs, config.code_vocab_size, dtype=torch.float, device=device)
-    batch_image = torch.ones(bs, config.n_channels, config.image_dim, config.image_dim, dtype=torch.float, device=device)
+    batch_image = torch.ones(bs, config.n_channels, config.image_dim_gen, config.image_dim_gen, dtype=torch.float, device=device)
+    batch_codes = []
     for i, n in enumerate(images):
         codes, image = n
         batch_context[i, codes] = 1
         batch_image[i] = load_image(image)
+        batch_codes.append(codes)
         
-    return batch_context, batch_image
+    return batch_context, batch_image, batch_codes
   
 def tensor_to_image(tensor):
     # First de-normalize from [-1, 1] to [0, 1]
@@ -52,13 +56,21 @@ def tensor_to_image(tensor):
   
 model = DiffusionModel(config).to(device)
 print("Loading previous model")
-checkpoint = torch.load(f'/srv/local/data/bpt3/MediSim/model_image_gen', map_location=torch.device(device))
-model.load_state_dict(checkpoint['model'])
+model.load_state_dict(torch.load(f'./save/model_image_gen', map_location=torch.device(device))['model'])
 
 # Generate Samples
 sampleIdx = np.random.choice(len(test_dataset), size=NUM_SAMPLES)
 sample_data = [test_dataset[i] for i in sampleIdx]
-sample_contexts, real_images = get_batch(sample_data, 0, NUM_SAMPLES)
+sample_contexts, real_images, real_codes = get_batch(sample_data, 0, NUM_SAMPLES)
+for i in tqdm(range(NUM_SAMPLES)):
+    with open(f'results/image_generation/realFinding_{i}.txt', 'w') as f:
+        codes = real_codes[i]
+        for code in codes:
+            finding = indexToCode[code]
+            if finding[2] in [0,1]:
+                f.write(f"{indexToCode[code]}\n")
+    
+
 for i in tqdm(range(NUM_SAMPLES)):
     img = tensor_to_image(real_images[i].cpu())
     img.save(f'results/image_generation/realImage_{i}.jpg')
