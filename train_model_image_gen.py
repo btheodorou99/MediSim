6d6359abf278c7ec0fdcef38c605cb17683670e3
btheodorou99 <99348934+btheodorou99@users.noreply.github.com>
2023-11-13 10:23:41 -0600

    Update train_model_image_gen.py

--- train_model_image_gen.py
+++ train_model_image_gen.py
@@ -10,7 +10,7 @@ from config_image import MediSimConfig
 from model_image import DiffusionModel
 
 SEED = 4
-cudaNum = 7
+cudaNum = 5
 random.seed(SEED)
 np.random.seed(SEED)
 torch.manual_seed(SEED)
@@ -26,6 +26,7 @@ val_dataset = pickle.load(open('./data_image/valDataset.pkl', 'rb'))
 val_dataset = [v for p in val_dataset for v in p['visits']]
 val_dataset = [(v[0], v[1]) for v in val_dataset]
 image_transform = transforms.Compose([
+        transforms.Resize((config.image_dim_gen, config.image_dim_gen)),
         transforms.ToTensor(),
         transforms.Lambda(lambda x: 2*x - 1)  # Normalize to [-1, 1]
     ])
@@ -38,7 +39,7 @@ def get_batch(dataset, loc, batch_size):
     images = dataset[loc:loc+batch_size]
     bs = len(images)
     batch_context = torch.zeros(bs, config.code_vocab_size, dtype=torch.float, device=device)
-    batch_image = torch.zeros(bs, config.n_channels, config.image_dim, config.image_dim, dtype=torch.float, device=device)
+    batch_image = torch.zeros(bs, config.n_channels, config.image_dim_gen, config.image_dim_gen, dtype=torch.float, device=device)
     for i, n in enumerate(images):
         codes, image = n
         batch_context[i, codes] = 1
@@ -71,37 +72,30 @@ for e in tqdm(range(config.epoch_gen)):
         loss.backward()
         optimizer.step()
         train_losses.append(loss.cpu().detach().item())
-        
-        if i % (250*config.batch_size_gen) == 0:
-            print("Epoch %d, Iter %d: Training Loss:%.6f"%(e, i, np.mean(train_losses)))
-            train_losses = []
-        if i % (500*config.batch_size_gen) == 0:
-            if i == 0:
-                continue
     
-            model.eval()
-            with torch.no_grad():
-                val_losses = []
-                for v_i in range(0, len(val_dataset), config.batch_size_gen):
-                    batch_context, batch_images = get_batch(val_dataset, v_i, config.batch_size_gen)                 
-                    val_loss, _ = model(batch_context, batch_images, gen_loss=True)
-                    val_losses.append((val_loss).cpu().detach().item())
-                
-                cur_val_loss = np.mean(val_losses)
-                print("Epoch %d Validation Loss:%.7f"%(e, cur_val_loss))
-                # if cur_val_loss < global_loss:
-                #     curr_patience = 0
-                #     global_loss = cur_val_loss
-                state = {
-                    'model': model.state_dict(),
-                    'optimizer': optimizer.state_dict()
-                }
-                torch.save(state, f'./save/model_image_gen')
-                print('\n------------ Save best model ------------\n')
-                # else:
-                #     curr_patience += 1
-                #     if curr_patience >= config.patience:
-                #         print("Early stopping")
-                #         break
+    model.eval()
+    with torch.no_grad():
+        val_losses = []
+        for v_i in range(0, len(val_dataset), config.batch_size_gen):
+            batch_context, batch_images = get_batch(val_dataset, v_i, config.batch_size_gen)                 
+            val_loss, _ = model(batch_context, batch_images, gen_loss=True)
+            val_losses.append((val_loss).cpu().detach().item())
+        
+        cur_val_loss = np.mean(val_losses)
+        print("Epoch %d Validation Loss:%.7f"%(e, cur_val_loss))
+        # if cur_val_loss < global_loss:
+        #     curr_patience = 0
+        #     global_loss = cur_val_loss
+        state = {
+            'model': model.state_dict(),
+            'optimizer': optimizer.state_dict()
+        }
+        torch.save(state, f'./save/model_image_gen')
+        print('\n------------ Save best model ------------\n')
+        # else:
+        #     curr_patience += 1
+        #     if curr_patience >= config.patience:
+        #         print("Early stopping")
+        #         break
 
-            model.train()
+    model.train()
